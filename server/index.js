fd8a451e11210f20513ff4dbb78527dcb60c051d
Mitchell Crane <mitchellhcrane@gmail.com>
2021-07-27 07:29:05 -0600

    added js file to public

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 const port = process.env.PORT || 4005;
 
